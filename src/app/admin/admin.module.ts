749c74e6f024569719ba3de4c12b87d3b1cb20bc
Mertcan Karataş <can.mert.karatas@gmail.com>
2022-10-20 15:40:52 +0300

    admin module add export

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from './layout/layout.module';
+import { ComponentsModule } from './components/components.module';
 
 
 
@@ -11,7 +12,8 @@ import { LayoutModule } from './layout/layout.module';
     LayoutModule
   ],
   exports:[
-    LayoutModule
+    LayoutModule,
+    ComponentsModule
   ]
 })
 export class AdminModule { }
