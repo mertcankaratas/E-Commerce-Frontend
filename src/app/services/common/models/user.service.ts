a0a0cb1b0acb49c235e05a97ed09ab6c93a0b187
Mertcan Karataş <can.mert.karatas@gmail.com>
2022-12-30 23:16:22 +0300

    Jwt access token

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,3 +1,6 @@
+import { TokenResponse } from './../../../contracts/Token/token-response';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './../../ui/custom-toastr.service';
+import { Token } from './../../../contracts/Token/token';
 import { Observable, firstValueFrom } from 'rxjs';
 import { CreateUser } from './../../../contracts/users/create-user';
 import { Injectable } from '@angular/core';
@@ -9,7 +12,7 @@ import { HttpClientService } from '../http-client.service';
 })
 export class UserService {
 
-  constructor(private httpClientService:HttpClientService) { }
+  constructor(private httpClientService:HttpClientService,private toastrService:CustomToastrService) { }
 
  async create(user:User):Promise<CreateUser>{
    const observable:Observable<CreateUser | User> =  this.httpClientService.post<CreateUser | User>({
@@ -19,13 +22,21 @@ export class UserService {
     return await firstValueFrom(observable) as CreateUser ;
   }
 
- async login(userNameOrEmail:string,password:string,callBackFunction?:()=>void):Promise<void>{
-   const observable: Observable<any>= this.httpClientService.post({
+ async login(userNameOrEmail:string,password:string,callBackFunction?:()=>void):Promise<any>{
+   const observable: Observable<any | TokenResponse>= this.httpClientService.post<any | TokenResponse>({
       controller:"users",
       action:"login"
     },{userNameOrEmail,password});
 
-    await firstValueFrom(observable);
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+
+    if(tokenResponse){
+      localStorage.setItem("accessToken",tokenResponse.token.accessToken);
+      this.toastrService.message("Kullanıcı girişi başarıyla sağlanmıştır","Giriş Başarılı",{
+        messageType:ToastrMessageType.Success,
+        position:ToastrPosition.TopRight
+      });
+    }
     callBackFunction();
   }
 }
