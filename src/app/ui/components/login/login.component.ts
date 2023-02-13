686c4b907c2a12eb3e46c2b1525bf475a35ebb95
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-02-13 17:42:46 +0300

    user auth service added

--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SocialAuthService, SocialUser, FacebookLoginProvider } from '@abacritt/angularx-social-login';
+import { UserAuthService } from 'src/app/services/common/models/user-auth.service';
 
 @Component({
   selector: 'app-login',
@@ -15,20 +16,20 @@ import { SocialAuthService, SocialUser, FacebookLoginProvider } from '@abacritt/
 })
 export class LoginComponent extends BaseComponent implements OnInit {
 
-  constructor(private userService:UserService,spinner:NgxSpinnerService,private authService:AuthService,private activatedRoute:ActivatedRoute,private router:Router,private socialAuthService:SocialAuthService ) {
+  constructor(private userAuthService:UserAuthService,spinner:NgxSpinnerService,private authService:AuthService,private activatedRoute:ActivatedRoute,private router:Router,private socialAuthService:SocialAuthService ) {
     super(spinner);
     socialAuthService.authState.subscribe(async (user: SocialUser)=>{
       console.log(user);
       this.showSpinner(SpinnerType.BallAtom);
       switch (user.provider) {
         case "GOOGLE":
-          await userService.googleLogin(user, () => {
+          await userAuthService.googleLogin(user, () => {
             this.authService.identityCheck();
             this.hideSpinner(SpinnerType.BallAtom);
           })
           break;
         case "FACEBOOK":
-          await userService.facebookLogin(user, () => {
+          await userAuthService.facebookLogin(user, () => {
             this.authService.identityCheck();
             this.hideSpinner(SpinnerType.BallAtom);
           })
@@ -43,7 +44,7 @@ export class LoginComponent extends BaseComponent implements OnInit {
 
  async login(userNameOrEmail:string,password:string){
   this.showSpinner(SpinnerType.BallAtom);
-    await this.userService.login(userNameOrEmail,password,()=>{
+    await this.userAuthService.login(userNameOrEmail,password,()=>{
       this.authService.identityCheck();
       this.activatedRoute.queryParams.subscribe(params=>{
        const returnUrl:string= params["returnUrl"];
