4827538056eb8c967972cb0197e531ceeec53590
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-03-07 17:01:16 +0300

    ui products pagination added

--- src/app/ui/components/products/list/list.component.ts
+++ src/app/ui/components/products/list/list.component.ts
@@ -11,23 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  currentPageNo:number;
-  products: ListProduct[];
-  constructor(private productService:ProductService,private activatedRoute:ActivatedRoute) { }
+  currentPageNo: number;
+  totalProductCount: number;
+  totalPageCount: number;
+  pageSize: number = 12;
+  pageList: number[] = [];
 
-  ngOnInit()  {
-    this.activatedRoute.params.subscribe(async params=>{
+  products: ListProduct[];
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(async params => {
 
       this.currentPageNo = parseInt(params["pageNo"] ?? 1);
-      const data :{totalProductCount: Number,products:ListProduct[]} = await this.productService.read(this.currentPageNo-1,12,
-        ()=>{
+      const data: { totalProductCount: number, products: ListProduct[] } = await this.productService.read(this.currentPageNo - 1, 12,
+        () => {
 
-      },
-      errorMessage=>{
+        },
+        errorMessage => {
 
-      });
+        });
 
       this.products = data.products;
+      this.totalProductCount = data.totalProductCount;
+      this.totalPageCount = Math.ceil(this.totalProductCount / this.pageSize);
+
+      this.pageList = [];
+      if (this.currentPageNo - 3 <= 0) {
+        for (let i = 1; i <= 7; i++) {
+          this.pageList.push(i);
+        }
+      }
+      else if (this.currentPageNo + 3 >= this.totalPageCount) {
+
+        for (let i = this.totalPageCount - 6; i <= this.totalPageCount; i++) {
+          this.pageList.push(i);
+        }
+
+      }
+
+      else {
+        for (let i = this.currentPageNo-3; i <= this.currentPageNo+3; i++) {
+          this.pageList.push(i);
+        }
+      }
     });
 
 
