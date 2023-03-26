dd13e133d00c224772f7dc8db52e6c9e345be0ec
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-03-26 17:53:39 +0300

    basket service added

---
+++ src/app/contracts/basket/update-basket-item.ts
@@ -0,0 +1,4 @@
+export class UpdateBasketItem{
+  baksetItemId:string;
+  quantity:number;
+}
--- src/app/services/common/models/basket.service.ts
+++ src/app/services/common/models/basket.service.ts
@@ -1,9 +1,51 @@
+import { UpdateBasketItem } from './../../../contracts/basket/update-basket-item';
+import { CreateBasketItem } from './../../../contracts/basket/create-basket-item';
+import { HttpClient } from '@angular/common/http';
+import { Observable, firstValueFrom } from 'rxjs';
+import { HttpClientService } from './../http-client.service';
 import { Injectable } from '@angular/core';
+import { ListBasketItem } from 'src/app/contracts/basket/list-basket-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
 
-  constructor() { }
+  constructor(private httpClientService:HttpClientService) { }
+
+
+async get():Promise<ListBasketItem[]>{
+  const observable : Observable<ListBasketItem[]> = this.httpClientService.get({
+    controller:"baskets"
+  });
+
+ return await firstValueFrom(observable)
+}
+
+async add(product:CreateBasketItem):Promise<any>{
+  const observable:Observable<any> = this.httpClientService.post({
+    controller:"baskets"
+  },product);
+
+  await firstValueFrom(observable);
+}
+
+
+async put(basketItem:UpdateBasketItem):Promise<void>{
+  const observable:Observable<any> = this.httpClientService.put({
+    controller:"baskets",
+  },basketItem);
+  await firstValueFrom(observable);
+}
+
+
+async remove(basketItemId:string){
+  const observable:Observable<any> = this.httpClientService.delete({
+    controller:"baskets"
+  },basketItemId);
+
+
+  await  firstValueFrom(observable);
+}
+
 }
