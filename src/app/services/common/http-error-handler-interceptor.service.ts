e7934f39c3f3cedca6b75a48990d7fa4578e1bc3
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-03-28 17:19:07 +0300

    http error interceptor refactor

--- src/app/services/common/http-error-handler-interceptor.service.ts
+++ src/app/services/common/http-error-handler-interceptor.service.ts
@@ -1,3 +1,6 @@
+import { SpinnerType } from './../../base/base.component';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserAuthService } from 'src/app/services/common/models/user-auth.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from './../ui/custom-toastr.service';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpStatusCode } from '@angular/common/http';
@@ -9,52 +12,67 @@ import { catchError, Observable, of } from 'rxjs';
 })
 export class HttpErrorHandlerInterceptorService implements HttpInterceptor {
 
-  constructor(private toastrService:CustomToastrService,private userAuthService:UserAuthService) { }
+  constructor(private toastrService: CustomToastrService, private userAuthService: UserAuthService, private router: Router, private spinner:NgxSpinnerService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
 
-    return next.handle(req).pipe(catchError(error=>{
+    return next.handle(req).pipe(catchError(error => {
       switch (error.status) {
         case HttpStatusCode.Unauthorized:
-          this.toastrService.message("Bu işlemi yapmaya yetkiniz bulunmamaktadır.","Yetkisiz işlem!",{
-            messageType:ToastrMessageType.Warning,
-            position:ToastrPosition.BottomFullWidth
-          });
 
-          this.userAuthService.refreshTokenLogin(localStorage.getItem("refreshToken")).then(data=>{
+          const url = this.router.url;
+
+          if (url == "/products") {
+
+            this.toastrService.message("Sepete ürün eklemek için oturum açmanız gerekiyor.", "Oturum Açınız!", {
+              messageType: ToastrMessageType.Warning,
+              position: ToastrPosition.TopRight
+            });
+
+          } else {
+            this.toastrService.message("Bu işlemi yapmaya yetkiniz bulunmamaktadır.", "Yetkisiz işlem!", {
+              messageType: ToastrMessageType.Warning,
+              position: ToastrPosition.BottomFullWidth
+            });
+
+
+          }
+
+          this.userAuthService.refreshTokenLogin(localStorage.getItem("refreshToken")).then(data => {
 
           });
 
           break;
 
         case HttpStatusCode.InternalServerError:
-          this.toastrService.message("Sunucuya Erişilmiyor!.","Sunucu hatası!",{
-            messageType:ToastrMessageType.Warning,
-            position:ToastrPosition.BottomFullWidth
+          this.toastrService.message("Sunucuya Erişilmiyor!.", "Sunucu hatası!", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
           });
           break;
 
 
         case HttpStatusCode.BadRequest:
-          this.toastrService.message("Geçersiz istek yapıldı.","Geçersiz istek!",{
-            messageType:ToastrMessageType.Warning,
-            position:ToastrPosition.BottomFullWidth
+          this.toastrService.message("Geçersiz istek yapıldı.", "Geçersiz istek!", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
           });
           break;
 
         case HttpStatusCode.NotFound:
-          this.toastrService.message("Sayfa bulunamadı.","Sayfa bulunamadı!",{
-            messageType:ToastrMessageType.Warning,
-            position:ToastrPosition.BottomFullWidth
+          this.toastrService.message("Sayfa bulunamadı.", "Sayfa bulunamadı!", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
           });
           break;
         default:
-          this.toastrService.message("Beklenmedik bir hata meydana gelmiştir.","Hata!",{
-            messageType:ToastrMessageType.Warning,
-            position:ToastrPosition.BottomFullWidth
+          this.toastrService.message("Beklenmedik bir hata meydana gelmiştir.", "Hata!", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
           });
           break;
       }
+      this.spinner.hide(SpinnerType.BallAtom);
       return of(error);
     }));
   }
